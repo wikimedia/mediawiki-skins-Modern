b7cac8475ef26e4f9dcc85478bddf38122214823
Isarra <zhorishna@gmail.com>
2018-03-18 16:58:44 +0000

    Remove MonoBook dependency by copying required functions directly into ModernTemplate
    ModernTemplate previously extended MonoBookTemplate,
    creating an undocumented dependency on MonoBook. This
    removes that dependency and allows more drastic changes to
    MonoBook (or Modern) itself without breaking things.
    
    Change-Id: I6259be98bfcf383615e930a25f8f990a553ef365

--- ModernTemplate.php
+++ ModernTemplate.php
@@ -24,7 +24,7 @@
  * @todo document
  * @ingroup Skins
  */
-class ModernTemplate extends MonoBookTemplate {
+class ModernTemplate extends BaseTemplate {
 
 	/**
 	 * Template filter callback for Modern skin.
@@ -174,5 +174,209 @@ class ModernTemplate extends MonoBookTemplate {
 	</body>
 </html>
 		<?php
+	}
+
+	/**
+	 * Prints all sidebar boxes
+	 *
+	 * @param array $sidebar
+	 */
+	protected function renderPortals( $sidebar ) {
+		if ( !isset( $sidebar['SEARCH'] ) ) {
+			$sidebar['SEARCH'] = true;
+		}
+		if ( !isset( $sidebar['TOOLBOX'] ) ) {
+			$sidebar['TOOLBOX'] = true;
+		}
+		if ( !isset( $sidebar['LANGUAGES'] ) ) {
+			$sidebar['LANGUAGES'] = true;
+		}
+
+		foreach ( $sidebar as $boxName => $content ) {
+			if ( $content === false ) {
+				continue;
+			}
+
+			// Numeric strings gets an integer when set as key, cast back - T73639
+			$boxName = (string)$boxName;
+
+			if ( $boxName == 'SEARCH' ) {
+				$this->searchBox();
+			} elseif ( $boxName == 'TOOLBOX' ) {
+				$this->toolbox();
+			} elseif ( $boxName == 'LANGUAGES' ) {
+				$this->languageBox();
+			} else {
+				$this->customBox( $boxName, $content );
+			}
+		}
+	}
+
+	/**
+	 * Prints the search
+	 */
+	function searchBox() {
+		?>
+		<div id="p-search" class="portlet" role="search">
+			<h3><label for="searchInput"><?php $this->msg( 'search' ) ?></label></h3>
+
+			<div id="searchBody" class="pBody">
+				<form action="<?php $this->text( 'wgScript' ) ?>" id="searchform">
+					<input type="hidden" name="title" value="<?php $this->text( 'searchtitle' ) ?>"/>
+					<?php echo $this->makeSearchInput( [ 'id' => 'searchInput' ] ); ?>
+
+					<?php
+					echo $this->makeSearchButton(
+						'go',
+						[ 'id' => 'searchGoButton', 'class' => 'searchButton' ]
+					);
+
+					if ( $this->config->get( 'UseTwoButtonsSearchForm' ) ) {
+						?>&#160;
+						<?php echo $this->makeSearchButton(
+							'fulltext',
+							[ 'id' => 'mw-searchButton', 'class' => 'searchButton' ]
+						);
+					} else {
+						?>
+
+						<div><a href="<?php
+						$this->text( 'searchaction' )
+						?>" rel="search"><?php $this->msg( 'powersearch-legend' ) ?></a></div><?php
+					} ?>
+
+				</form>
+
+				<?php $this->renderAfterPortlet( 'search' ); ?>
+			</div>
+		</div>
+	<?php
+	}
+
+	/**
+	 * Prints the content actions (cactions) bar.
+	 */
+	function cactions() {
+		?>
+		<div id="p-cactions" class="portlet" role="navigation">
+			<h3><?php $this->msg( 'views' ) ?></h3>
+
+			<div class="pBody">
+				<ul><?php
+					foreach ( $this->data['content_actions'] as $key => $tab ) {
+						echo '
+				' . $this->makeListItem( $key, $tab );
+					} ?>
+
+				</ul>
+				<?php $this->renderAfterPortlet( 'cactions' ); ?>
+			</div>
+		</div>
+	<?php
+	}
+
+	/**
+	 * Prints the toolbox
+	 */
+	function toolbox() {
+		?>
+		<div class="portlet" id="p-tb" role="navigation">
+			<h3><?php $this->msg( 'toolbox' ) ?></h3>
+
+			<div class="pBody">
+				<ul>
+					<?php
+					foreach ( $this->getToolbox() as $key => $tbitem ) {
+						?>
+						<?php echo $this->makeListItem( $key, $tbitem ); ?>
+
+					<?php
+					}
+					// Avoid PHP 7.1 warnings
+					$skin = $this;
+					Hooks::run( 'MonoBookTemplateToolboxEnd', [ &$skin ] );
+					Hooks::run( 'SkinTemplateToolboxEnd', [ &$skin, true ] );
+					?>
+				</ul>
+				<?php $this->renderAfterPortlet( 'tb' ); ?>
+			</div>
+		</div>
+	<?php
+		Hooks::run( 'MonoBookAfterToolbox' );
+	}
+
+	/**
+	 * Prints the other languages box
+	 */
+	function languageBox() {
+		if ( $this->data['language_urls'] !== false ) {
+			?>
+			<div id="p-lang" class="portlet" role="navigation">
+				<h3<?php $this->html( 'userlangattributes' ) ?>><?php $this->msg( 'otherlanguages' ) ?></h3>
+
+				<div class="pBody">
+					<ul>
+						<?php foreach ( $this->data['language_urls'] as $key => $langLink ) { ?>
+							<?php echo $this->makeListItem( $key, $langLink ); ?>
+
+						<?php
+						}
+						?>
+					</ul>
+
+					<?php $this->renderAfterPortlet( 'lang' ); ?>
+				</div>
+			</div>
+		<?php
+		}
+	}
+
+	/**
+	 * Prints user-defined navigation boxes
+	 *
+	 * @param string $bar
+	 * @param array|string $cont
+	 */
+	function customBox( $bar, $cont ) {
+		$portletAttribs = [
+			'class' => 'generated-sidebar portlet',
+			'id' => Sanitizer::escapeId( "p-$bar" ),
+			'role' => 'navigation'
+		];
+
+		$tooltip = Linker::titleAttrib( "p-$bar" );
+		if ( $tooltip !== false ) {
+			$portletAttribs['title'] = $tooltip;
+		}
+		echo '	' . Html::openElement( 'div', $portletAttribs );
+		$msgObj = wfMessage( $bar );
+		?>
+
+		<h3><?php echo htmlspecialchars( $msgObj->exists() ? $msgObj->text() : $bar ); ?></h3>
+		<div class="pBody">
+			<?php
+			if ( is_array( $cont ) ) {
+				?>
+				<ul>
+					<?php
+					foreach ( $cont as $key => $val ) {
+						?>
+						<?php echo $this->makeListItem( $key, $val ); ?>
+
+					<?php
+					}
+					?>
+				</ul>
+			<?php
+			} else {
+				# allow raw HTML block to be defined by extensions
+				print $cont;
+			}
+
+			$this->renderAfterPortlet( $bar );
+			?>
+		</div>
+		</div>
+	<?php
 	}
 }
